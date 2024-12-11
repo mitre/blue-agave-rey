c4677aa6a623fb8b61918e609928380df1ed8d34
Michael Carenzo <79934822+mikecarenzo@users.noreply.github.com>
2024-12-11 18:51:08 -0500

    implement search camera

--- src/components/Panels/Search.vue
+++ src/components/Panels/Search.vue
@@ -1,7 +1,13 @@
 <template>
   <div class="activity-set-search-panel" @contextmenu.stop>
-    <SearchField id="search-field" :fields="fields" @search="onSearch"></SearchField>
-    {{ results }}
+    <SearchField id="search-field" :fields="fields" @search="onSearch">
+      <span class="search-instrumentation" v-if="results.length">
+        {{ result + 1 }} / {{ results.length }}
+      </span>
+    </SearchField>
+    <div v-for="r of results" :key="r.ref" @click="moveCameraTo(r.ref)">
+      {{ r }}
+    </div>
   </div>
 </template>
 
@@ -9,8 +15,8 @@
 import Features from "@/assets/rey.features";
 import * as Store from "@/store/StoreTypes";
 // Dependencies
-import { mapState } from "vuex";
-import { defineComponent, type PropType } from "vue";
+import { defineComponent } from "vue";
+import { mapActions, mapState } from "vuex";
 import type { Index } from "lunr";
 // Components
 import SearchField from "@/components/Controls/SearchField.vue";
@@ -20,7 +26,9 @@ export default defineComponent({
   data() {
     return {
       fields: new Set(Features.activity_set_event_listing.indexed_keys),
-      results: [] as Index.Result[]
+      result: -1,
+      results: [] as Index.Result[],
+      queryString: ""
     }
   },
   computed: {
@@ -40,18 +48,49 @@ export default defineComponent({
   methods: {
 
     /**
+     * Activity Sets Store actions
+     */
+    ...mapActions("ActivitySetsStore", [
+      "unselectAll",
+      "selectItem",
+      "triggerCameraFocus"
+    ]),
+
+    /**
      * Performs a search.
      * @param queryString
      *  The query string. 
      */
     onSearch(queryString: string) {
       try {
-        this.results = this.searchIndex.search(queryString);
+        if(queryString !== this.queryString) {
+          this.queryString = queryString;
+          this.results = this.searchIndex.search(queryString);
+          this.result = this.results.length ? 0 : -1;
+        } else {
+          this.result = (this.result + 1) % this.results.length;
+        }
+        if(this.result !== -1) {
+          this.moveCameraTo(this.results[this.result].ref);
+        }
         console.log(this.results);
       } catch(err) {
         console.log("Search Failed.");
         console.log(err);
       }
+    },
+
+    /**
+     * Moves camera to a node.
+     * @param id
+     *  The node's id.
+     */
+    async moveCameraTo(id: string) {
+      // Select items
+      await this.unselectAll();
+      await this.selectItem(id);
+      // Trigger camera
+      await this.triggerCameraFocus();
     }
 
   },
@@ -65,6 +104,10 @@ export default defineComponent({
 
 .activity-set-search-panel {
   padding: 15px 12px;
+}
+
+.search-instrumentation {
+  margin-right: 10px;
 }
 
 </style>
